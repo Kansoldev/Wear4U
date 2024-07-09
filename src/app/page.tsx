f21c98d88147f64e07e3ac2e1cd49e630641c3b1
oyinkansola <yahayaoyinkansola@gmail.com>
2024-07-09 09:15:19 +0100

    Added men and women collection area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,6 +123,128 @@ export default function Home() {
           <button>Shop</button>
         </div>
       </section>
+
+      <section className="w-[1200px] max-w-full mx-auto mt-14">
+        <span
+          className="text-[#a74107] text-4xl block tracking-[-4px] text-center"
+          style={{ fontFamily: "Helvetica" }}
+        >
+          men
+        </span>
+
+        <h2
+          className="text-center uppercase text-5xl sm:text-7xl lg:text-8xl xl:text-[166px] tracking-[-4px] font-normal"
+          style={{
+            fontFamily: "helvetica",
+          }}
+        >
+          Collection
+        </h2>
+
+        <div className="product__grid grid2">
+          <div
+            className="p-4 border border-solid border-[#bdbdbd] hidden xl:block"
+            style={{ gridArea: "bg-1" }}
+          >
+            <img className="collection_image" src="/men.jpg" alt="" />
+          </div>
+
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className={`product text-center xl:text-left ${
+                product.id !== 1 ? "mt-4 xl:mt-0" : ""
+              }`}
+            >
+              <div className="love-icon flex justify-center items-center p-3">
+                <span className="lnr lnr-heart"></span>
+              </div>
+
+              <div className="product-image xl:mt-10">
+                <img
+                  src={product.imgUrl}
+                  className="mx-auto xl:mx-0 xl:mb-8"
+                  alt=""
+                />
+              </div>
+
+              <div className="product-desc">
+                <h3 className="font-medium uppercase">{product.name}</h3>
+                <p className="font-bold mt-[0.625rem]">${product.price}</p>
+              </div>
+
+              <div className="add-to-cart-icon flex justify-center items-center p-3">
+                <span className="lnr lnr-cart font-bold"></span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="w-[1200px] max-w-full mx-auto mt-14">
+        <span
+          className="text-[#a74107] text-4xl block tracking-[-4px] text-center"
+          style={{ fontFamily: "Helvetica" }}
+        >
+          women
+        </span>
+
+        <h2
+          className="text-center uppercase text-5xl sm:text-7xl lg:text-8xl xl:text-[166px] tracking-[-4px] font-normal"
+          style={{
+            fontFamily: "helvetica",
+          }}
+        >
+          Collection
+        </h2>
+
+        <div className="product__grid">
+          <div
+            className="p-4 border border-solid border-[#bdbdbd] hidden xl:block"
+            style={{ gridArea: "bg-1" }}
+          >
+            <img className="collection_image" src="/women.jpg" alt="" />
+          </div>
+
+          {products.map((product) => (
+            <div
+              key={product.id}
+              className={`product text-center xl:text-left ${
+                product.id !== 1 ? "mt-4 xl:mt-0" : ""
+              }`}
+            >
+              <div className="love-icon flex justify-center items-center p-3">
+                <span className="lnr lnr-heart"></span>
+              </div>
+
+              <div className="product-image xl:mt-10">
+                <img
+                  src={product.imgUrl}
+                  className="mx-auto xl:mx-0 xl:mb-8"
+                  alt=""
+                />
+              </div>
+
+              <div className="product-desc">
+                <h3 className="font-medium uppercase">{product.name}</h3>
+                <p className="font-bold mt-[0.625rem]">${product.price}</p>
+              </div>
+
+              <div className="add-to-cart-icon flex justify-center items-center p-3">
+                <span className="lnr lnr-cart font-bold"></span>
+              </div>
+            </div>
+          ))}
+
+          <Link
+            href={"#"}
+            className="mt-[0.625rem] hidden lg:inline border-0 bg-transparent font-bold text-right no-underline text-black"
+            style={{ fontFamily: "Helvetica", gridArea: "bg-6" }}
+          >
+            see all
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
