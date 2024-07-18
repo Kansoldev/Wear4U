1718def1dfbaa069e509f8a917a2f939b9e3f1f8
oyinkansola <yahayaoyinkansola@gmail.com>
2024-07-18 14:16:46 +0100

    remove API Key env variable

--- src/api/product/route.ts
+++ src/api/product/route.ts
@@ -3,7 +3,7 @@
 export async function getAllProducts() {
   try {
     const res = await fetch(
-      `https://api.timbu.cloud/products?organization_id=732bbf6844ae44d38f267592f5fc24a8&Apikey=${process.env.TIMBU_API_KEY}&Appid=02XVAXBXJIHEHKA`,
+      `https://api.timbu.cloud/products?organization_id=732bbf6844ae44d38f267592f5fc24a8&Apikey=9cff8f503a9a4d5aa28f6ddbe265416320240712174819475966&Appid=02XVAXBXJIHEHKA`,
       {
         next: {
           revalidate: 3600,
